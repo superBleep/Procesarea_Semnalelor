2910dec0bcd62cf3f4ce31144a73f3c6cc66b117
nubaz <lucapaulf@gmail.com>
2023-10-19 00:45:00 +0300

    Laboratorul 1 v2

--- Lab_1_PS.py
+++ Lab_1_PS.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 def ex1():
     # Pct. (a)
-    time = np.linspace(0, 0.3, 1000)
+    time = np.linspace(0, 0.03, 1000)
 
     # Pct. (b)
     fig, axs = plt.subplots(3)
@@ -18,33 +18,40 @@ def ex1():
 
     for ax in axs.flat:
         ax.grid(linestyle='--')
+        ax.set_xlabel('Timp (t)')
+        ax.set_ylabel('Amplitudine (A)')
 
     # Pct. (c)
-    sample_nr = 50
-    sample_period = 1 / 200
-
-    samples = np.arange(sample_nr)
-
-    sample_int = samples * sample_period
+    sample_nr = np.int8(0.03 * 200)
+    samples = np.linspace(0, 0.03, sample_nr)
 
     fig2, axs2 = plt.subplots(3)
-    fig2.suptitle('Semnale eșantionate ({} de eșantioane, 200 Hz)'.format(sample_nr))
-    axs2[0].stem(samples, np.cos(520 * np.pi * sample_int + np.pi / 3), 'r')
-    axs2[1].stem(samples, np.cos(280 * np.pi * sample_int - np.pi / 3), 'g')
-    axs2[2].stem(samples, np.cos(120 * np.pi * sample_int + np.pi / 3), 'b')
+    fig2.suptitle('Semnale eșantionate ({} eșantioane, 200 Hz)'.format(sample_nr))
+
+    axs2[0].stem(samples, np.cos(520 * np.pi * samples + np.pi / 3), 'r')
+    axs2[1].stem(samples, np.cos(280 * np.pi * samples - np.pi / 3), 'g')
+    axs2[2].stem(samples, np.cos(120 * np.pi * samples + np.pi / 3), 'b')
+
+    axs2[0].plot(time, np.cos(520 * np.pi * time + np.pi / 3), 'r')
+    axs2[1].plot(time, np.cos(280 * np.pi * time - np.pi / 3), 'g')
+    axs2[2].plot(time, np.cos(120 * np.pi * time + np.pi / 3), 'b')
 
     for ax in axs2.flat:
         ax.grid(linestyle='--')
+        ax.set_xlabel("Esantion")
+        ax.set_ylabel("Amplitudine (A)")
 
     plt.show()
 
 def ex2():
     # Pct. (a)
-    samples = np.linspace(0, 0.05, 1600)
+    samples = np.linspace(0, 0.03, 1600)
 
-    plt.figure(1)    
+    plt.figure(1)
     plt.stem(samples, np.sin(2 * np.pi * 400 * samples), 'r')
-    plt.title('Semnal sinusoidal (f = 400 Hz, t = 0:0.05, 1600 de eșantioane)')
+    plt.title('Semnal sinusoidal (f = 400 Hz, t = 0:0.03, 1600 de eșantioane)')
+    plt.xlabel('Esantion')
+    plt.ylabel('Amplitudine (A)')
     plt.grid(linestyle='--')
 
     # Pct. (b)
@@ -53,29 +60,37 @@ def ex2():
     plt.figure(2)
     plt.plot(time, np.sin(2 * np.pi * 800 * time), 'r')
     plt.title('Semnal sinusoidal (f = 800 Hz, t = 0:3)')
+    plt.xlabel('Timp (t)')
+    plt.ylabel('Amplitudine (A)')
     plt.grid(linestyle='--')
 
     # Pct. (c)
-    time = np.linspace(0, 0.3, 1000)
+    time = np.linspace(0, 0.02, 1000)
 
     plt.figure(3)
     plt.plot(time, 2 * (time * 240 - np.floor(1 / 2 + time * 240)), color='purple')
-    plt.title('Semnal sawtooth (f = 240 Hz, t = 0:0.3)')
+    plt.title('Semnal sawtooth (f = 240 Hz, t = 0:0.02)')
+    plt.xlabel('Timp (t)')
+    plt.ylabel('Amplitudine (A)')
     plt.grid(linestyle='--')
 
     # Pct. (d)
-    time = np.linspace(0, 3, 1000)
+    time = np.linspace(0, 0.02, 1000)
 
     plt.figure(4)
     plt.plot(time, np.sign(np.sin(2 * np.pi * 300 * time)), 'g')
-    plt.title('Semnal square (f = 300 Hz, t = 0:3)')
-    plt.grid()
+    plt.title('Semnal square (f = 300 Hz, t = 0:0.02)')
+    plt.xlabel('Timp (t)')
+    plt.ylabel('Amplitudine (A)')
+    plt.grid(linestyle='--')
 
     # Pct. (e)
     arr = np.random.rand(128, 128)
 
     plt.figure(5)
     plt.imshow(arr)
+    plt.xlabel('Semnal 1')
+    plt.ylabel('Semnal 2')
     plt.title('Semnal 2D aleator (128x128)')
 
     # Pct. (f)
@@ -86,7 +101,9 @@ def ex2():
 
     plt.figure(6)
     plt.imshow(arr)
-    plt.title('Semnal 2D')
+    plt.xlabel('Semnal 1')
+    plt.ylabel('Semnal 2')
+    plt.title('Semnal 2D (128x128)')
 
     plt.show()
 
@@ -102,7 +119,7 @@ def ex2():
 
 def main():
     ex1()
-    # ex2()
+    ex2()
 
 if __name__ == '__main__':
     main()
