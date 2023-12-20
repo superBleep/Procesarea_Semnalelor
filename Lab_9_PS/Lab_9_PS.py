ec903618d4c9c2bf60573d6cd1191a2257037906
nubaz <lucapaulf@gmail.com>
2023-12-20 17:55:30 +0200

    Laboratorul 9 v2

--- Lab_9_PS/Lab_9_PS.py
+++ Lab_9_PS/Lab_9_PS.py
@@ -21,52 +21,69 @@ def ex1():
     return series
 
 
-def ex2():
-    x = ex1()
+def ex2(x):
     n = np.size(x)
 
-    s = np.empty(n)
+    s = np.empty(n) # Aplicarea medierei exponentiale
     s[0] = x[0]
-    alfa = 0.2
+    alfa_og = 0.2 # Alfa fixat
 
     for i in range(1, n):
-        s[i] = alfa * x[i] + (1 - alfa) * s[i - 1]
+        s[i] = alfa_og * x[i] + (1 - alfa_og) * s[i - 1]
 
-    sTuned = np.empty(n)
+    sTuned = np.empty(n) # Mediere exponentiala cu alfa dinamic
     sTuned[0] = x[0]
 
     for i in range(1, n):
-        a = np.empty(i)
-        b = np.empty(i)
+        a = []
+        b = []
+        for j in range(i):
+            a.append(x[j] - s[j - 1])
+            b.append(x[j + 1] - s[j - 1])
 
-        for j in range(1, i):
-            a[j - 1] = x[j] - s[j - 1]
-        
-        for j in range(2, i):
-            b[j - 2] = x[j + 1] - s[j - 1]
-
-        alfa = np.dot(a, b) / np.dot(a, a)
+        alfa = np.dot(a, b) / np.linalg.norm(a)
 
         sTuned[i] = alfa * x[i] + (1 - alfa) * sTuned[i - 1]
 
+    _, axs = plt.subplots(3, figsize=(6, 7))
+    plt.suptitle('Serii de timp nenormalizate')
+    plt.subplots_adjust(hspace=0.655)
 
-    plt.figure(1)
+    axs[0].plot(np.arange(n), x, 'r')
+    axs[0].set_title('Seria originală')
 
-    plt.plot(np.arange(n), x, label='Seria originală')
-    plt.plot(np.arange(n), s, label='Rez. medierii exponențiale')
-    plt.plot(np.arange(n), sTuned, label='Rez. medierii exponențiale (alfa dinamic)')
+    axs[1].plot(np.arange(n), s, 'g')
+    axs[1].set_title(f'Rez. medierei exponențiale (alfa={alfa_og})')
 
-    plt.title('Serii de timp nenormalizate')
-    plt.xlabel('Timp (t)')
-    plt.ylabel('Valori')
-    plt.legend()
-    plt.grid(linestyle='--')
+    axs[2].plot(np.arange(n), sTuned, 'purple')
+    axs[2].set_title('Rez. medierei exponențiale (alfa dinamic)')
+
+    for ax in axs.flat:
+        ax.set_xlabel('Timp (t)')
+        ax.set_ylabel('Valori')
+        ax.grid(linestyle='--')
 
     plt.show()
 
 
+def ex3(x):
+    n = np.size(x)
+    q = 50 # Orizontul modelului MA
+
+    y = []
+    for i in range(n):
+        if i < q:
+            y.append(0)
+        else:
+            # todo
+            pass
+
+
 def main():
-    ex2()
+    x = ex1() # Seria de timp
+
+    ex2(x)
+    ex3(x)
 
 
 if __name__ == '__main__':
